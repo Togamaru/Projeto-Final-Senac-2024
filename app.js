33b8975d046bdcb5dbb989ed6ffbb1ae89e07e8f
Marcio Macedo de Araújo <44624503+MarcioMa@users.noreply.github.com>
2024-05-01 03:24:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const userRoutes = require("./src/routes/userRoutes");
+const bodyParser = require('body-parser');
+
 
 app.use(express.json());
 app.use(cors())
 app.use("/user", userRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 module.exports = app;
