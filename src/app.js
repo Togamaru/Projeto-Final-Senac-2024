3585bd854712a4ab1be13c5f765c9d96c036d090
Marcio Macedo de Araújo <44624503+MarcioMa@users.noreply.github.com>
2024-05-07 21:13:32 -0300

    Atualizado 07/05 21:13

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ app.use(bodyParser.json());
 app.use(cors())
 // Rotas
 const routes = require('./routes/routes');
+const userRoutes = require('./routes/userRoutes');
 
 
 app.use('/login', routes);
-app.use('/registre', routes);
+app.use('/register', routes);
+app.use("/user", userRoutes);
 // app.use('/outra-rota', outraRota);
 
 module.exports = app;
